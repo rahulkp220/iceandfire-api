65f0ddce5442bcfbf18d97c51f65dba77479bbda
rahul <rahulkp220@gmail.com>
2019-05-28 18:38:42 +0530

    Fixing testcases, using reverse for URLs

--- book/tests.py
+++ book/tests.py
@@ -1,17 +1,13 @@
 import json
-from django.test import TestCase, Client
-from rest_framework.test import APIClient, APITestCase
+from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
 
 from .views import BookViewset, ExternalBookView
 from .models import Book, Author
 
 
-# Create your tests here.
 class ExternalBookViewTest(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        client = Client()
 
     def test_get_without_data(self):
         response = self.client.get('/api/external-books/?name=Test')
@@ -45,13 +41,12 @@ class ExternalBookViewTest(TestCase):
 
 
 class BookViewset(APITestCase):
-    client = APIClient()
 
     def setUp(self):
-        author = Author(name="Test Author")
-        author.save()
+        self.author = Author(name="Test Author")
+        self.author.save()
 
-        book = Book(
+        self.book = Book(
             name="Test Book",
             isbn="123456788",
             number_of_pages=888,
@@ -59,24 +54,24 @@ class BookViewset(APITestCase):
             country="India",
             release_date="2019-05-28"
         )
-        book.save()
-        book.authors.add(author)
+        self.book.save()
+        self.book.authors.add(self.author)
 
     def test_create(self):
         test_data = {
             "name": "Sample Book",
             "isbn": "123456789",
-            "authors":[{
-                "name":"Sample Author"
+            "authors": [{
+                "name": "Sample Author"
             }],
             "number_of_pages": 999,
             "publisher": "Sample Publisher",
             "country": "India",
-            "release_date":"2019-05-28"
+            "release_date": "2019-05-28"
         }
         response = self.client.post(
-            '/api/v1/books/', 
-            data=json.dumps(test_data), 
+            reverse('v1:books-list'),
+            data=json.dumps(test_data),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, 200)
@@ -98,14 +93,14 @@ class BookViewset(APITestCase):
         })
 
     def test_list(self):
-        response = self.client.get('/api/v1/books/')
+        response = self.client.get(reverse('v1:books-list'))
         self.assertEqual(response.status_code, 200)
         self.assertDictEqual(response.json(), {
             "status_code": 200,
             "status": "success",
             "data": [
                 {
-                    "id": 1,
+                    "id": self.book.id,
                     "name": "Test Book",
                     "isbn": "123456788",
                     "authors": [
@@ -120,13 +115,13 @@ class BookViewset(APITestCase):
         })
 
     def test_retrieve(self):
-        response = self.client.get('/api/v1/books/1/')
+        response = self.client.get(reverse('v1:books-detail', kwargs={'pk': self.book.id}))
         self.assertEqual(response.status_code, 200)
         self.assertDictEqual(response.json(), {
             "status_code": 200,
             "status": "success",
             "data": {
-                "id": 1,
+                "id": self.book.id,
                 "name": "Test Book",
                 "isbn": "123456788",
                 "authors": [
@@ -143,16 +138,16 @@ class BookViewset(APITestCase):
         patch_data = {
             "name": "Patch Book",
             "isbn": "123456788",
-            "authors":[{
-                "name":"Test Author"
+            "authors": [{
+                "name": "Test Author"
             }],
             "number_of_pages": 999,
             "publisher": "Test Publisher",
             "country": "India",
-            "release_date":"2019-05-28"
+            "release_date": "2019-05-28"
         }
         response = self.client.patch(
-            '/api/v1/books/1/',
+            reverse('v1:books-detail', kwargs={'pk': self.book.id}),
             data=json.dumps(patch_data), 
             content_type='application/json'
         )
@@ -161,7 +156,7 @@ class BookViewset(APITestCase):
             "status_code": 200,
             "status": "success",
             "data": {
-                "id": 1,
+                "id": self.book.id,
                 "name": "Patch Book",
                 "isbn": "123456788",
                 "authors": [
@@ -175,7 +170,7 @@ class BookViewset(APITestCase):
         })
 
     def test_destroy(self):
-        response = self.client.delete('/api/v1/books/1/')
+        response = self.client.delete(reverse('v1:books-detail', kwargs={'pk': self.book.id}))
         self.assertEqual(response.status_code, 200)
         self.assertDictEqual(response.json(), {
             'status_code': 204,
