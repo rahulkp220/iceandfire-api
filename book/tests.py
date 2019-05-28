6f9c1ebd56d479d1c22e055a906ab5fe3e8c8783
rahul lakhanpal <rahulkp220@gmail.com>
2019-05-28 16:31:04 +0530

    Fixed testcases

--- book/tests.py
+++ book/tests.py
@@ -1,8 +1,9 @@
 import json
 from django.test import TestCase, Client
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 
 from .views import BookViewset, ExternalBookView
+from .models import Book, Author
 
 
 # Create your tests here.
@@ -43,8 +44,23 @@ class ExternalBookViewTest(TestCase):
             })
 
 
-class BookViewset(TestCase):
+class BookViewset(APITestCase):
     client = APIClient()
+
+    def setUp(self):
+        author = Author(name="Test Author")
+        author.save()
+
+        book = Book(
+            name="Test Book",
+            isbn="123456788",
+            number_of_pages=888,
+            publisher="Test Publisher",
+            country="India",
+            release_date="2019-05-28"
+        )
+        book.save()
+        book.authors.add(author)
 
     def test_create(self):
         test_data = {
@@ -68,7 +84,7 @@ class BookViewset(TestCase):
             "status_code": 201,
             "status": "success",
             "data": {
-                "id": 1,
+                "id": 2,
                 "name": "Sample Book",
                 "isbn": "123456789",
                 "authors": [
@@ -84,15 +100,86 @@ class BookViewset(TestCase):
     def test_list(self):
         response = self.client.get('/api/v1/books/')
         self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.json(), {
+            "status_code": 200,
+            "status": "success",
+            "data": [
+                {
+                    "id": 1,
+                    "name": "Test Book",
+                    "isbn": "123456788",
+                    "authors": [
+                        "Test Author"
+                    ],
+                    "number_of_pages": 888,
+                    "publisher": "Test Publisher",
+                    "country": "India",
+                    "release_date": "2019-05-28"
+                }
+            ]
+        })
 
     def test_retrieve(self):
         response = self.client.get('/api/v1/books/1/')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.json(), {
+            "status_code": 200,
+            "status": "success",
+            "data": {
+                "id": 1,
+                "name": "Test Book",
+                "isbn": "123456788",
+                "authors": [
+                    "Test Author"
+                ],
+                "number_of_pages": 888,
+                "publisher": "Test Publisher",
+                "country": "India",
+                "release_date": "2019-05-28"
+            }
+        })
 
     def test_patch(self):
-        response = self.client.get('/api/v1/books/1/')
-        self.assertEqual(response.status_code, 404)
+        patch_data = {
+            "name": "Patch Book",
+            "isbn": "123456788",
+            "authors":[{
+                "name":"Test Author"
+            }],
+            "number_of_pages": 999,
+            "publisher": "Test Publisher",
+            "country": "India",
+            "release_date":"2019-05-28"
+        }
+        response = self.client.patch(
+            '/api/v1/books/1/',
+            data=json.dumps(patch_data), 
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.json(), {
+            "status_code": 200,
+            "status": "success",
+            "data": {
+                "id": 1,
+                "name": "Patch Book",
+                "isbn": "123456788",
+                "authors": [
+                    "Test Author"
+                ],
+                "number_of_pages": 999,
+                "publisher": "Test Publisher",
+                "country": "India",
+                "release_date": "2019-05-28"
+            }
+        })
 
     def test_destroy(self):
-        response = self.client.get('/api/v1/books/1/')
-        self.assertEqual(response.status_code, 404)
+        response = self.client.delete('/api/v1/books/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.json(), {
+            'status_code': 204,
+            'status': 'success',
+            'message': 'The book Test Book was deleted successfully',
+            'data': []
+        })
